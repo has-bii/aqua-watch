68b8a268effcb94b9aecb68b63529b198f8fee82
Hasbiy Robbiy <hasbii.rofficial@gmail.com>
2024-12-16 01:08:57 +0300

    style

--- src/components/topnav.tsx
+++ src/components/topnav.tsx
@@ -3,13 +3,17 @@ import WifiStatus from "./wifi-status";
 
 export default function TopNav() {
   return (
-    <div className="w-full py-6 border-b h-fit shadow-sm">
-      <div className="container mx-auto flex items-center justify-between">
+    <div className="w-full border-b h-[72px] shadow-sm flex items-center">
+      <div className="container mx-auto flex items-center px-4">
         <h1 className="text-lg font-black uppercase">Aqua Watch</h1>
 
-        <div className="inline-flex gap-4 items-center">
+        <div className="inline-flex gap-8 items-center mx-auto">
           <Link to="/" className="[&.active]:font-bold">
             Home
+          </Link>
+
+          <Link to="/getting-started" className="[&.active]:font-bold">
+            Get Started
           </Link>
         </div>
 
